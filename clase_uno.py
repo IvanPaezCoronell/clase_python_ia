5ca933afa20cd08bf1d164074c21414bc29914fc
Ivan Paez Coronell <ipaez1@cuc.edu.co>
2021-08-30 20:14:55 -0500

    Tipos de datos, Conversiones y Concatenaciones

--- clase_uno.py
+++ clase_uno.py
@@ -7,3 +7,103 @@ Created on Mon Aug 30 18:56:18 2021
 """
 
 print('Hello World')
+
+# VARIABLES
+a = 3
+print(type(a))
+a = "Hola"
+print(type(a))
+a = 3.5
+print(type(a))
+a = True
+print(type(a))
+
+# OPERACIONES
+a = 5
+b = 2
+c = a + b
+print(c)
+
+a = 5
+b = 2
+c = a - b
+print(c)
+
+a = 5
+b = 2
+c = a * b
+print(c)
+
+a = 5
+b = 2
+c = a / b
+print(c)
+
+a = 5
+b = 2
+c = a // b
+print(c)
+
+# potencia
+a = 5
+b = 2
+c = a ** b
+print(c)
+
+# raiz
+a = 9
+c = a ** (1/3)
+print(c)
+
+"""import math
+raiz = math.sqrt(25)
+print(raiz)"""
+
+# Tipos de Datos
+
+# string str
+a = "Hola Mundo"
+a = 'Hola Mundo'
+b = "I can't do it"
+c = 'Alias "Paez"'
+
+# entero int
+a = 5
+
+# decimal float
+a = 5.6
+
+# booleano bool
+x = True
+y = False
+
+# CONVERSIONES ENTRE TIPOS DE DATOS
+
+# convertir de x a entero
+a = '3'
+y = int(a)
+print(y)
+print(type(y))
+
+# convertir de x a decimal
+a = '3'
+y = float(a)
+print(y)
+print(type(y))
+
+# convertir de x a string
+a = '3'
+y = str(a)
+print(y)
+print(type(y))
+
+# CONCATENACIONES
+a = 'hola'
+b = 'mundo'
+c = a +' '+ b
+
+a = 'hola'
+b = a * 5
+
+
+
